c11ec90d5161103af1b18e5360f9f2cf6a41aa11
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-10-24 12:50:14 +0300

    📣🚀💻👨‍💻✨🐣⌚️🔋🔌 ИСПРАВИЛ Н Е Д О Ч Ё Т

--- src/pages/api/addConference.js
+++ src/pages/api/addConference.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient
 export default async function addConference(req, res) {
     const { name, start } = req.body;
 
-    const conference = await prisma.conference.create({
+    await prisma.conference.create({
         data: {
             name: name,
             start: start
